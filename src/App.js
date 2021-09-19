f3473bdc3737b5983114e5ad86a49282967a433b
palmswill <59519523+palmswill@users.noreply.github.com>
2021-09-19 15:05:29 -0400

    change fetch location

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'./react-resume2021/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
